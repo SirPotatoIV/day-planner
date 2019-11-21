66c7556af2ffac0db3f31e5c013b7035a164af86
SirPotatoIV <otoolej33@gmail.com>
2019-11-20 22:18:19 -0600

    Changed variable names in the function for getting the current date and time to make them more readable. Converted the moment object into the correct date formant and the current hour. Combineded them into a new variable as an object. Added a return at the end of the function to return the object with the current date and hour.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,18 +3,36 @@ function initiatePlanner() {
 
     // Get current date and time
     // -- Homework calls for getting this using Moment.js
-    function currentDateTime() {
-        const UNIXdate = moment();
-        console.log("Date gotten from Moment.js: "+ UNIXdate);
-        const formattedDate = UNIXdate.format("dddd, MMMM Do");
-        console.log("UNIX date formatted using Moment.js: "+formattedDate);
+    function currentDateHour() {
+        // All moment() and format() is coming from Moment.js library
+        
+        // Gets current time and date from Moment.js. Moment returns an object full of all the necessary required information.
+        // const momentObj = moment();
+        // Converts the converts moment object into the correct date format. Day spelled out, Month Spelled out, and day in number format. Thursday, November 20th
+        // const currentDate = momentObj.format("dddd, MMMM Do");
+        const currentDate = "Wednesday, November 20th";
+        // Converts the moment object into the current hour from 1-24
+        // const currentHour = momentObj.format("k");
+        const currentHour = "22";
+        const current = {
+            date: currentDate,
+            hour: currentHour
+        };
+
+        console.log(current);
+        return current;
+        
     }
-    currentDateTime(); 
+    currentDateHour(); 
     
+    // Update header to show current date
+    function showCurrentDate() {
+
+    }
+    showCurrentDate();
     // Check for any previously created events in local storage.
     // -- If they exist store in array
     // -- If they don't exist set array equal to an empty array
-    // Update header to show current date
     // Render single day calendar view with a row per hour from the hours of 9 AM to 5 PM
     // -- Top row should be 
     // -- Each day row should contain 3 columns. Col 1: the hour, Col 2: an input/display of event, and Col 3: a button with a save icon
