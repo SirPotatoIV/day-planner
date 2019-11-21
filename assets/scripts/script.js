a4768a431c19e7c0ccdb52fd6a4be6fcf3a7438c
SirPotatoIV <otoolej33@gmail.com>
2019-11-20 21:24:38 -0600

    Added psuedo code to script.js.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,14 +1,20 @@
+// Function to contain all other functions
 function initiatePlanner() {
-    // Recommended by axios documentation to provide autocomplete and parameter typings
-    // const axios = require('axios').default;
-    console.log("script.js is working.")
 
-    axios.get('https://pokeapi.co/api/v2/pokemon/charmander')
-  .then(function (response) {
-    // handle success
-    console.log(response);
-  })
-
+    // Get current date and time
+    // -- Homework calls for getting this using Moment.js
+    // Check for any previously created events in local storage.
+    // -- If they exist store in array
+    // -- If they don't exist set array equal to an empty array
+    // Update header to show current date
+    // Render single day calendar view with a row per hour from the hours of 9 AM to 5 PM
+    // -- Top row should be 
+    // -- Each day row should contain 3 columns. Col 1: the hour, Col 2: an input/display of event, and Col 3: a button with a save icon
+    // -- Render current col 2 in row of hours that have passed gray, current hour red, and future hours green
+    // -- Render any previously created/stored events from local storage data that was gotten.
+    // Start event listener for the save button in every row
+    // -- If save button clicked, save value in input and store it in local storage
+    // -- Will probably have to make the default text of the input change to the inputted text to make it visible if the page is refreshed
 
 }
 initiatePlanner();
